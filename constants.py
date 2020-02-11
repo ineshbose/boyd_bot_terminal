00fc802e01e32b05922d321a7d8a822d2fc5562d
ineshbose <ineshbose@yahoo.com>
2020-02-11 14:24:02 +0000

    Another commit

--- constants.py
+++ constants.py
@@ -1,2 +1,3 @@
 URL = "https://www.gla.ac.uk/apps/timetable/#/login"
 chromedriver = 'C:/Users/Inesh/Downloads/chromedriver'
+weekdayMapping = {"MONDAY":0, "TUESDAY":1, "WEDNESDAY":2, "THURSDAY":3, "FRIDAY":4}
--- scraper.py
+++ scraper.py
@@ -1,28 +1,13 @@
-'''
-from bs4 import BeautifulSoup
-import requests
-
-page = requests.get()
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.get_text())
-
-week = soup.find(id='seven-day-forecast-list').find_all(class_='tombstone-container')
-
-for i in range(len(week)):
-    print(week[i].get_text())
-'''
 from selenium import webdriver
-from constants import URL, chromedriver
+from constants import URL, chromedriver, weekdayMapping
 from selenium.common.exceptions import UnexpectedAlertPresentException, NoSuchElementException, ElementNotInteractableException
-from selenium.webdriver.support.ui import WebDriverWait
-import time
+import time, datetime
 from getpass import getpass
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 browser = webdriver.Chrome(chromedriver, chrome_options=options)
-weekdayMapping = {"MONDAY":0, "TUESDAY":1, "WEDNESDAY":2, "THURSDAY":3, "FRIDAY":4}
 
 def login():
     browser.get(URL)
@@ -53,24 +38,35 @@ def read_today():
     time.sleep(1)
     #classes = browser.find_elements_by_class_name("fc-title")
     classes = browser.find_elements_by_class_name("fc-time-grid-event.fc-event.fc-start.fc-end")
-    print("\nYou have..")
-    for clas in classes:
-        try:
-            clas.click()
-            time.sleep(1)
-            table = browser.find_element_by_class_name("dialogueTable")
-            print(table.text, "\n")
-            browser.find_element_by_class_name("close.text-white").click()
-        except ElementNotInteractableException as e:
-            print("(Unable to fetch class)")
-            continue
+    if classes == []:
+        print("Either there are no classes, or something went wrong.")
+    else:
+        print("\nYou have..")
+        for clas in classes:
+            try:
+                clas.click()
+                time.sleep(1)
+                table = browser.find_element_by_class_name("dialogueTable")
+                print(table.text, "\n")
+                browser.find_element_by_class_name("close.text-white").click()
+            except ElementNotInteractableException as e:
+                print("(Unable to fetch class)\n")
+                continue
     #for clas in classes:
         #print(clas.text)
         #print(" ")
 
-def read_tomorrow():
-    browser.find_element_by_class_name("fc-next.button.fc-button.fc-button-primary").click()
+def specific_day():
+    date_entry = input('Enter a date in DD-MM-YYYY format: ')
+    day, month, year = map(int, date_entry.split('-'))
+    date1 = datetime.date(year, month, day)
+    loop_days((date1 - datetime.date.today()).days)
+
+def loop_days(n):
+    for i in range(n):
+        browser.find_element_by_class_name("fc-next-button.fc-button.fc-button-primary").click()
     read_today()
+    browser.find_element_by_class_name("fc-today-button.fc-button.fc-button-primary").click()
 
 def read_week():
     time.sleep(1)
@@ -84,30 +80,29 @@ def read_week():
     days.append(data.split("Wednesday")[1].split("Thursday")[0])
     days.append(data.split("Thursday")[1].split("Friday")[0])
     days.append(data.split("Friday")[1])
-    print(days[weekdayMapping[input("\nWhat day? ").upper()]])
+    print(days[weekdayMapping[input("\nWhat day this week? ").upper()]])
+    browser.find_element_by_class_name("fc-timeGridDay-button.fc-button.fc-button-primary").click()
 
 def main():
     print("\nHello! Give me a minute to initialize..\n")
     login()
     quit="n"
     while quit.upper()!="Y":
-        print("\nWhat's up?\n1 - Today\n2 - This Week\n3 - X days later")
+        print("\nWhat's up?\n1 - Today\n2 - This Week\n3 - X days later\n4 - On Specific Day")
         choice = int(input("Input: "))
         if choice == 1:
             read_today()
         elif choice == 2:
             read_week()
         elif choice == 3:
-            for i in range(int(input("How many days after? "))):
-                browser.find_element_by_class_name("fc-next-button.fc-button.fc-button-primary").click()
-            read_today()
+            loop_days(int(input("How many days? ")))
+        elif choice == 4:
+            specific_day()
         else:
             print("Invalid input.")
         quit=input("Quit? [Y/N]: ")
 
     print("\nClosing browser..\n")
     browser.quit()
-
-
 
 main()

44a906b58daff83d14b390aa17ff217b5a9eff2d
ineshbose <ineshbose@yahoo.com>
2020-03-06 18:49:40 +0000

    read_now() implemented along with many improvements

--- scraper.py
+++ scraper.py
@@ -7,9 +7,11 @@ from selenium.webdriver.common.by import By
 import time, datetime
 from getpass import getpass
 
+
 URL = "https://www.gla.ac.uk/apps/timetable/#/login"
 chromedriver = path = os.path.dirname(os.path.realpath(__file__)) + '/chromedriver'
 weekdayMapping = {"MONDAY":0, "TUESDAY":1, "WEDNESDAY":2, "THURSDAY":3, "FRIDAY":4}
+
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -17,58 +19,111 @@ options.add_argument('--disable-gpu')
 options.add_argument('--log-level=3')
 browser = webdriver.Chrome(chromedriver, options=options)
 
+
 def login():
     browser.get(URL)
     browser.find_element_by_id("guid").send_keys(input("GUID: "))
     browser.find_element_by_id("password").send_keys(getpass())
     print("\nLogging in..\n")
     browser.find_element_by_xpath("//*[@id='app']/div/main/button").click()
+    
     try:
         element_present = EC.presence_of_element_located((By.XPATH, "//*[@id='app']/div/div[1]/div[1]/a"))
         WebDriverWait(browser, 4).until(element_present)
         browser.find_element_by_xpath("//*[@id='app']/div/div[1]/div[1]/a").click()
         if browser.current_url == "https://www.gla.ac.uk/apps/timetable/#/timetable":
             print("\nLogin successful!\n")
+    
     except exceptions.UnexpectedAlertPresentException as e:
         print("\nInvalid credentials! Try again.\n")
         browser.refresh()
         login()
+    
     except exceptions.NoSuchElementException as load:
         print("\nSomething went wrong. Maybe the connection was too slow. Try again.\n")
         browser.refresh()
         login()
 
+
 def read_today():
     element_present = EC.visibility_of_all_elements_located((By.CLASS_NAME, "fc-time-grid-event.fc-event.fc-start.fc-end"))
-    WebDriverWait(browser, 1).until(element_present)
-    classes = browser.find_elements_by_class_name("fc-time-grid-event.fc-event.fc-start.fc-end")
-    if classes == []:
-        print("Either there are no classes, or something went wrong.")
-    else:
+    
+    try:
+        WebDriverWait(browser, 1).until(element_present)
+        classes = browser.find_elements_by_class_name("fc-time-grid-event.fc-event.fc-start.fc-end")
         print("\nYou have..")
+    
         for clas in classes:
             try:
                 clas.click()
                 element_present = EC.visibility_of_element_located((By.CLASS_NAME, "dialogueTable"))
                 WebDriverWait(browser, 1).until(element_present)
                 table = browser.find_element_by_class_name("dialogueTable")
-                print(table.text, "\n")
+                class_data = []
+               
+                for i in range(1,8):
+                    class_data.append(browser.find_element_by_xpath("//*[@id='eventModal']/div/div/div[2]/table/tr[{}]/td".format(str(i))).text) 
+               
+                print((class_data[0] + " ({}) ".format(class_data[2]) + "\nfrom {} to {} ".format(class_data[4],class_data[5]) + "\nat {}.".format(class_data[1])) + "\n\n")
                 browser.find_element_by_class_name("close.text-white").click()
+            
             except exceptions.ElementNotInteractableException as e:
                 print("(Unable to fetch class)\n")
                 continue
+    
+    except exceptions.TimeoutException:
+        print("There seem to be no classes.")
+
+
+def read_now():
+    element_present = EC.visibility_of_all_elements_located((By.CLASS_NAME, "fc-time-grid-event.fc-event.fc-start.fc-end"))
+    
+    try:
+        WebDriverWait(browser, 1).until(element_present)
+        classes = browser.find_elements_by_class_name("fc-time-grid-event.fc-event.fc-start.fc-end")
+        print("\nUp next, you have..")
+    
+        for clas in classes:
+            try:
+                clas.click()
+                element_present = EC.visibility_of_element_located((By.CLASS_NAME, "dialogueTable"))
+                WebDriverWait(browser, 1).until(element_present)
+                table = browser.find_element_by_class_name("dialogueTable")
+                class_data = []
+    
+                for i in range(1,8):
+                    class_data.append(browser.find_element_by_xpath("//*[@id='eventModal']/div/div/div[2]/table/tr[{}]/td".format(str(i))).text) 
+                
+                tyme = str(datetime.datetime.now().date()) + " {}".format(class_data[4])
+                classtime = datetime.datetime.strptime(tyme, '%Y-%m-%d %I:%M %p')
+    
+                if(datetime.datetime.now() <= classtime):
+                    print((class_data[0] + " ({}) ".format(class_data[2]) + "\nfrom {} to {} ".format(class_data[4],class_data[5]) + "\nat {}.".format(class_data[1])) + "\n\n")
+                    break
+                browser.find_element_by_class_name("close.text-white").click()
+    
+            except exceptions.ElementNotInteractableException as e:
+                print("(Unable to fetch class)\n")
+                continue
+    
+    except exceptions.TimeoutException:
+        print("There seem to be no classes.")
+
 
 def specific_day():
-    date_entry = input('Enter a date in DD-MM-YYYY format: ')
-    day, month, year = map(int, date_entry.split('-'))
+    date_entry = input('Enter a date in DD/MM/YYYY format: ')
+    day, month, year = map(int, date_entry.split('/'))
     date1 = datetime.date(year, month, day)
     loop_days((date1 - datetime.date.today()).days)
+
 
 def loop_days(n):
     for i in range(n):
         browser.find_element_by_class_name("fc-next-button.fc-button.fc-button-primary").click()
+    
     read_today()
     browser.find_element_by_class_name("fc-today-button.fc-button.fc-button-primary").click()
+
 
 def read_week():
     element_present = EC.visibility_of_element_located((By.CLASS_NAME, "fc-listWeek-button.fc-button.fc-button-primary"))
@@ -76,6 +131,7 @@ def read_week():
     browser.find_element_by_class_name("fc-listWeek-button.fc-button.fc-button-primary").click()
     element_present = EC.visibility_of_element_located((By.CLASS_NAME, "fc-list-table"))
     WebDriverWait(browser, 1).until(element_present)
+    
     week = browser.find_element_by_class_name("fc-list-table")
     data = week.text
     days = []
@@ -83,16 +139,18 @@ def read_week():
     days.append(data.split("Tuesday")[1].split("Wednesday")[0])
     days.append(data.split("Wednesday")[1].split("Thursday")[0])
     days.append(data.split("Thursday")[1].split("Friday")[0])
+    
     days.append(data.split("Friday")[1])
     print(days[weekdayMapping[input("\nWhat day this week? ").upper()]])
     browser.find_element_by_class_name("fc-timeGridDay-button.fc-button.fc-button-primary").click()
+
 
 def main():
     print("\nHello! Give me a minute to initialize..\n")
     login()
     quit="n"
     while quit.upper()!="Y":
-        print("\nWhat's up?\n1 - Today\n2 - This Week\n3 - X days later\n4 - On Specific Day")
+        print("\nWhat's up?\n1 - Today\n2 - This Week\n3 - X days later\n4 - On Specific Day\n5 - Up Next / Now")
         choice = int(input("Input: "))
         if choice == 1:
             read_today()
@@ -102,11 +160,14 @@ def main():
             loop_days(int(input("How many days? ")))
         elif choice == 4:
             specific_day()
+        elif choice == 5:
+            read_now()
         else:
             print("Invalid input.")
         quit=input("Quit? [Y/N]: ")
 
     print("\nClosing browser..\n")
     browser.quit()
+
 
 main()
